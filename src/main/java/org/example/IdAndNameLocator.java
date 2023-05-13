61af30ef75ee23911ace83bfd705fb0d9a731c12
jhumelbonganciso <jhumelbonganciso04@gmail.com>
2023-05-14 01:26:20 +0800

    added timeouts method to implicitly wait before adding certain action

--- src/main/java/org/example/IdAndNameLocator.java
+++ src/main/java/org/example/IdAndNameLocator.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class IdAndNameLocator {
     public static void main(String[] args) {
         System.setProperty("webdriver.chromer.driver","C:\\WebDrivers\\chromedriver.exe");
@@ -11,10 +13,14 @@ public class IdAndNameLocator {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("incorrectPassword");
         driver.findElement(By.className("signInBtn")).click();
+
+
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());;
 
     }
 }
